dc2f2ebc130376552ce50c68f793438d06d899ae
Fernando Pimmel <pimmelfernando@gmail.com>
2023-02-27 11:39:51 -0300

    posicionamento de iluminação

--- js/render/core/renderer.js
+++ js/render/core/renderer.js
@@ -44,7 +44,7 @@ export const ATTRIB_MASK = {
 
 const GL = WebGLRenderingContext; // For enums
 
-const DEF_LIGHT_DIR = new Float32Array([0.0, 0.0, 0.0]);
+const DEF_LIGHT_DIR = new Float32Array([-1.0, -1.0, -1.0]);
 const DEF_LIGHT_COLOR = new Float32Array([3.0, 3.0, 3.0]);
 
 const PRECISION_REGEX = new RegExp('precision (lowp|mediump|highp) float;');
